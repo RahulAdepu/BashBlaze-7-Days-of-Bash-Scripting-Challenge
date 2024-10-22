b59659df501c15877c535d404cb318ad31dd0a7e
Ubuntu <ubuntu@ip-172-31-18-66.us-east-2.compute.internal>
2024-10-22 03:24:08 +0000

    first solution

---
+++ Challenges/Day_1/answer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Task 1 ******************************
+
+<< Note 
+This is a multiline comment, we use >> char to start and end multiline comment with a unique word.
+Eg - Note
+
+Note
+
+#Task 2 ******************************
+
+<< note 
+echo is used to print the given string.
+Bash commands are case sensitive.
+
+note
+
+echo "What's your name ? "
+read name
+echo "Nice to meet you $name!"
+
+#Task 3 *******************************
+
+
